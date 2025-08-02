d1e7e61a1c7ff98dff54f5a24ea4f245dc168a6b
ssaafd <cgv5599@gmail.com>
2025-08-02 19:38:52 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Bienvenue dans Underground Studio</h1>
+      <p>Accède à <a href="/test.html">test.html</a> pour tester un stream audio.</p>
+    </div>
+  );
+}
